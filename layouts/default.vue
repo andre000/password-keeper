47f9ef8630325b6375059ba69f68344fe1722bfd
Andre <andre@dev-next.com>
2019-03-18 14:17:07 -0300

    :sparkles: Created basic layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,90 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <a-layout id="main-menu">
+    <a-layout-sider
+      v-model="collapsed"
+      breakpoint="sm"
+      collapsible
+    >
+      <div class="logo">
+        <template v-if="!collapsed">
+          Password Keeper
+        </template>
+        <template v-else>
+          PK
+        </template>
+      </div>
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="bars" />
+          <span>All Items</span>
+        </a-menu-item>
+        <a-menu-divider />
+        <a-menu-item v-if="!collapsed" class="menu-subtitle" disabled>
+          FOLDERS
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="()=> collapsed = !collapsed"
+        />
+      </a-layout-header>
+      <a-layout-content>
+        <nuxt />
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data: () => ({
+    collapsed: false,
+  }),
+};
+</script>
+<style scoped>
+.ant-layout-content {
+  margin: 24px 16px;
+  padding: 24px;
+  background: #fff;
+  min-height: 280px;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+#main-menu .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color .3s;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+#main-menu .trigger:hover {
+  color: #1890ff;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+#main-menu .logo {
+  height: 32px;
+  margin: 16px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-weight: bold;
+  color: white;
+  display: flex;
+  align-content: center;
+  justify-content: center;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+.ant-menu-item-divider {
+  background-color: #ffffff33;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.menu-subtitle {
+  font-variant: all-small-caps;
+  cursor: auto;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,9 @@ module.exports = {
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/,
+          options: {
+            fix: true,
+          },
         });
       }
     },
